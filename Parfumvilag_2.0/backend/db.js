e4f0d6dfa9841cfaa4d8bb3d457045a989ae4e73
mikilaczy <0114laczymiki@gmail.com>
2025-03-20 11:49:20 +0100

    port

--- Parfumvilag_2.0/backend/db.js
+++ Parfumvilag_2.0/backend/db.js
@@ -7,6 +7,7 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT || 3306, // Itt adod meg a portot
+  port: process.env.MYSQL_PORT || 3307, // Itt adod meg a portot
 });
 
 connection.connect((err) => {
